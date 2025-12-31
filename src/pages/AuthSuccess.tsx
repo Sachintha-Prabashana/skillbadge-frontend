f961fc30415da24e717e8a9929f1859a6868c2fa
Sachintha Prabashana <sachinthaprabhashana2003@gmail.com>
2025-12-31 14:27:26 +0530

    refactor: remove React import from AuthSuccess component

--- src/pages/AuthSuccess.tsx
+++ src/pages/AuthSuccess.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 
 import { Loader2 } from "lucide-react";
